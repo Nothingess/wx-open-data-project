0293aefc9e919a23548be238eb28bedbce87a31e
PythonC <602784923@qq.com>
2019-06-19 16:15:43 +0800

    修改为子域上传分数，获取主域openid

--- assets/launch.js
+++ assets/launch.js
@@ -8,8 +8,17 @@ cc.Class({
         content: cc.Node,
         prefab: cc.Prefab,
         selfBlock:cc.Node,
-        map:[],
-        score:[],
+
+        rankItems:{
+            default:[],
+            type:[cc.Texture2D]
+        },
+
+        _selfData:null,
+        _selfOpenId:0,
+        _currUserScore_lv1:0,           //第一关分数
+        _currUserScore_lv2:0,           //第二关分数
+        _currUserScore_lv3:0            //第三关分数
     },
 
     start () {
@@ -19,29 +28,45 @@ cc.Class({
         }
 
         wx.onMessage( data => {
-            if (data.message) {
-                console.log(data.message);
+            if (!!data) {
+                switch(data.k){
+                    case "rank_1":
+                    case "rank_2":
+                    case "rank_3":
+                        this.uploadScore(data);
+                    break;
+                    case "openid":
+                    break;
+                }
             }
         });
 
-        this.map = new Map();
-        this.score = [];
-        
-
         this.initUserInfo();
-        this.initFriendInfo();
+    },
+    /**上传分数 */
+    uploadScore(data){
+        let kvData = [{key:data.k, value:data.v}]
+        this.setUserCloudStorage(kvData);
+    },
+    /**获取openid */
+    getSelfOpenId(){
 
-/*         this.score.sort();
-        this.sortMap(); */
     },
 
+
     initUserInfo () {
+        wx.getUserCloudStorage({
+            keyList:["rank_1", "rank_2"],
+            success:(res)=>{
+                this._selfData = res.KVDataList;
+            }
+        })
         wx.getUserInfo({
             openIdList: ['selfOpenId'],
             lang: 'zh_CN',
             success: (res) => {
-                //this.getRankData(res.data);
                 this.showUserRank(res.data[0]);
+                this.initFriendInfo();
             },
             fail: (res) => {
                 console.error(res);
@@ -53,10 +78,7 @@ cc.Class({
         wx.getFriendCloudStorage({
             keyList:["rank_1", "rank_2"],
             success: (res) => {
-                for (let i = 0; i < res.data.length; ++i) {
-                    //this.getRankData(res.data[i]);
-                    this.createUserBlock(res.data[i]);
-                }
+                this.showFriendRank(this.sortList(res.data));
             },
             fail: (res) => {
                 console.error(res);
@@ -64,37 +86,28 @@ cc.Class({
         });
     },
 
-    getRankData(data){
-        let KVData = data.KVDataList;
-        KVData.forEach(element => {
-            if(element.key == "rank_1"){
-                let k = element.value + "_" + data.nickName;
-                this.map.set(k, data);
-                this.score.push(parseInt(element.value));
-            }
-        });
-    },
-
-    /**排序字典 */
-    sortMap(){
-        let rankdata = []
-        this.map.forEach(v, k => {
-            let kk = parseInt(k.splice('_')[0]);
-            while(rankdata[this.score.indexOf(kk)] != null){
-                kk++
-            }
-            rankdata[kk] = v;
-        });
-    },
-
     showUserRank(user){
+        let self = this;
         let userName = this.selfBlock.getChildByName('userName').getComponent(cc.Label);
         userName.string = user.nickName || user.nickname;
-
+        this._selfOpenId = user.openId;
         cc.loader.load({url: user.avatarUrl, type: 'png'}, (err, texture) => {
             if (err) console.error(err);
-            let userIcon = this.selfBlock.getChildByName('avatar').children[0].getComponent(cc.Sprite);
+            let userIcon = self.selfBlock.getChildByName('avatar').children[0].getComponent(cc.Sprite);
+            let scoreLa = self.selfBlock.getChildByName("score").getComponent(cc.Label);
             userIcon.spriteFrame = new cc.SpriteFrame(texture);
+            if(self._selfData != null){
+                self._selfData.forEach(element => {
+                    if(element.key == "rank_1"){
+                        scoreLa.string = element.value;
+                        self._currUserScore_lv1 = parseInt(element.value);
+                    }else if(element.key == "rank_2"){
+                        self._currUserScore_lv2 = parseInt(element.value);
+                    }else{
+                        self._currUserScore_lv3 = parseInt(element.value);
+                    }
+                });
+            }
         });
     },
 
@@ -113,6 +126,107 @@ cc.Class({
             let userIcon = node.getChildByName('avatar').children[0].getComponent(cc.Sprite);
             userIcon.spriteFrame = new cc.SpriteFrame(texture);
         });
-    }
+    },
 
+    showFriendRank(rankData){
+        let rank = 1;
+        rankData.forEach(element => {
+            let node = cc.instantiate(this.prefab);
+            node.parent = this.content;
+            node.x = 0;
+    
+            // set nickName
+            let userName = node.getChildByName('userName').getComponent(cc.Label);
+            userName.string = element.nickName || element.nickname;
+    
+            // set avatar
+            cc.loader.load({url: element.avatarUrl, type: 'png'}, (err, texture) => {
+                if (err) console.error(err);
+
+                let userIcon = node.getChildByName('avatar').children[0].getComponent(cc.Sprite);
+                userIcon.spriteFrame = new cc.SpriteFrame(texture);
+            });
+
+            let rankingLa = node.getChildByName("ranking").getComponent(cc.Label);
+            let scoreLa = node.getChildByName("score").getComponent(cc.Label);
+            rankingLa.string = rank < 4?`${rank}st`:`${rank}`;
+            node.getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(this.rankItems[rank - 1]);
+
+            if(element.openid == this._selfOpenId){//玩家本身
+                this.setSelfRank(rankingLa.string)
+            }
+            element.KVDataList.forEach(e => {
+                if(e.key == "rank_1"){
+                    scoreLa.string = e.value;
+                }
+            });
+            rank++;
+        });
+    },
+
+    /**设置玩家排名 */
+    setSelfRank(val){
+        this.selfBlock.getChildByName('ranking').getComponent(cc.Label).string = val;
+    },
+    /**上传分数 */
+    setUserCloudStorage(list){
+        if(!this.isMaxScore(list[0].key, list[0].value)){
+            console.log("不是历史最高分", list[0].value);
+            return;
+        }
+        wx.setUserCloudStorage({
+            KVDataList:list,
+            success:()=>{
+                console.log("上传数据成功！")
+            },
+            fail:()=>{
+                console.log("上传数据失败！")
+            }
+        })
+    },
+    /**当前分数是否是历史最高分数 */
+    isMaxScore(rank, score){
+        if(rank == "rank_1"){
+            if(this._currUserScore_lv1 <= 0)return true;
+            if(this._currUserScore_lv1 < score)return true;
+            return false;
+        }else if(rank == "rank_2"){
+            if(this._currUserScore_lv2 <= 0)return true;
+            if(this._currUserScore_lv3 < score)return true;
+            return false;
+        }else if(rank == "rank_3"){
+            if(this._currUserScore_lv3 <= 0)return true;
+            if(this._currUserScore_lv4 < score)return true;
+            return false;
+        }
+
+    },
+
+
+    sortList: function(ListData, order){ //排序(ListData：res.data;order:false降序，true升序)
+        ListData.sort(function(a,b){
+            var AMaxScore = 0;
+            var KVDataList = a.KVDataList;
+            for(var i = 0; i < KVDataList.length; i++){
+                if(KVDataList[i].key == "rank_1"){
+                AMaxScore = KVDataList[i].value;
+                }
+            }
+    
+            var BMaxScore = 0;
+            KVDataList = b.KVDataList;
+            for(var i = 0; i<KVDataList.length; i++){
+                if(KVDataList[i].key == "rank_1"){
+                BMaxScore = KVDataList[i].value;
+                }
+            }
+
+            if(order){
+                return parseInt(AMaxScore) - parseInt(BMaxScore);
+            }else{
+                return parseInt(BMaxScore) - parseInt(AMaxScore);
+            }
+        });
+        return ListData;
+    }
 });
